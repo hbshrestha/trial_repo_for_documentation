5db4045788e455606bedb0dec3f9e46f982af6fb
hbshrestha <himalayashrestha0@gmail.com>
2022-10-06 14:54:00 +0200

    update author and project name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Trial Documentation'
+copyright = '2022, HBS'
+author = 'HBS'
 
 release = '0.1'
 version = '0.1.0'
